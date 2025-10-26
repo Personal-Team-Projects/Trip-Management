cc4ea99e7cd6f2a5d4809b27696cf6ee265a69d4
sohag7th <sohaghssain7th@gmail.com>
2025-10-27 04:21:15 +0600

    Home page image problem solve 5

--- public/allImage.js
+++ public/allImage.js
@@ -1,4 +1,4 @@
-import homeBanner from "./home/homeBanner.png";
-import travelReady from "./home/travelReady.png";
+import homeBanner from "./Home/homeBanner.png";
+import travelReady from "./Home/travelReady.png";
 
 export { homeBanner, travelReady };
